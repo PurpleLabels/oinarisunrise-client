74d84cc8b3a6789e5dea0fbc02e0c20c2adea8e2
yuki_tsushima <tsuchinoko.kensetsu@gmail.com>
2022-01-15 18:42:50 +0900

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,3 +7,21 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+const { Nuxt } = require('nuxt');
+const express = require('express');
+
+const app = express();
+const config = {
+    dev: false,
+    srcDir: '.nuxt',
+    build: {
+      publicPath: '/assets/',
+    }
+  };
+
+const nuxt = new Nuxt(config);
+app.use(async (req: any, res: any) => {
+  // await nuxt.ready()
+  return await nuxt.render(req, res)
+})
+exports.ssrapp = functions.https.onRequest(app);
